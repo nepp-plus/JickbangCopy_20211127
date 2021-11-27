0d55229cd22b829e72055437f76b011b7cfc0286
nepp-plus <nepp.plus@gmail.com>
2021-11-27 13:44:21 +0900

    1억 미만인 경우 천단위 , 찍는 함수 추가

--- app/src/main/java/com/neppplus/jickbangcopy_20211127/datas/RoomData.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211127/datas/RoomData.kt
@@ -1,5 +1,8 @@
 package com.neppplus.jickbangcopy_20211127.datas
 
+import java.text.NumberFormat
+import java.util.*
+
 class RoomData(
 
     val price: Int,
@@ -10,6 +13,20 @@ class RoomData(
 ) {
 
 //    방 데이터의 기능 :  가격을 가공해서 String으로 주는 기능. => 리스트뷰 / 상세화면 두군데에서 사용.
+
+    fun getFormattedPrice() : String {
+
+        if (this.price < 10000) {
+
+//            ex. 8000 => 8,000
+
+            return NumberFormat.getNumberInstance( Locale.KOREA ).format(this.price)
+        }
+        else {
+            return "1억이상"
+        }
+
+    }
 
 //    추가 기능 : 층수도 가공해서 String으로 주자.
 
