315b460457acf0698fa24c8f861303031e64d4d4
nepp-plus <nepp.plus@gmail.com>
2021-11-27 15:50:27 +0900

    Handler 클래스 활용  - 2.5초 뒤에 메인으로 이동

--- app/src/main/java/com/neppplus/jickbangcopy_20211127/SplashActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211127/SplashActivity.kt
@@ -1,11 +1,34 @@
 package com.neppplus.jickbangcopy_20211127
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+//        2.5초 이후에 화면 전환으로 지연시켜보자. (핸들러)
+
+        val myHandler = Handler( Looper.getMainLooper() )  // 메인쓰레드 (UI 전담쓰레드) 소환
+
+        myHandler.postDelayed( {
+
+//           2.5초 뒤에 실행할 일 : 메인화면으로 이동하기
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+
+
+        }, 2500 )
+
+
+
+
     }
 }
