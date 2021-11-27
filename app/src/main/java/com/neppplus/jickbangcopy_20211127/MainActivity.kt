3e911c73a8f989e98476ff1466dd9c632b837dc4
nepp-plus <nepp.plus@gmail.com>
2021-11-27 14:21:23 +0900

    Serializable 활용 -> 방 데이터를 통째로 상세화면에 전달 / 활용

--- app/src/main/java/com/neppplus/jickbangcopy_20211127/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211127/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
             val clickedRoom  =  mRoomList[position]
 
             val myIntent = Intent( this, ViewRoomDetailActivity::class.java )
+            myIntent.putExtra("room", clickedRoom)
             startActivity(myIntent)
 
         }
