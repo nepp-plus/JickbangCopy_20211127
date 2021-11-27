aba17979cb59f1de859ac2b6ab547d5a38228f22
nepp-plus <nepp.plus@gmail.com>
2021-11-27 14:09:14 +0900

    방 목록 클릭시 -> 상세화면으로 단순 이동

--- app/src/main/java/com/neppplus/jickbangcopy_20211127/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211127/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.jickbangcopy_20211127
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20211127.adapters.RoomAdapter
@@ -29,6 +30,16 @@ class MainActivity : AppCompatActivity() {
 
         mRoomAdapter = RoomAdapter( this, R.layout.room_list_item, mRoomList )
         roomListView.adapter =  mRoomAdapter
+
+        roomListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val clickedRoom  =  mRoomList[position]
+
+            val myIntent = Intent( this, ViewRoomDetailActivity::class.java )
+            startActivity(myIntent)
+
+        }
+
 
     }
 }
